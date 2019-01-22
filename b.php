0331f443c8e5561799e92b5dbfcfb942f0d45238
allen1012 <921543658@qq.com>
2019-01-22 08:18:14 -0800

    add b.php

---
+++ b.php
@@ -0,0 +1,25 @@
+<?php
+	
+echo "Hello World!";
+
+$n = 11;
+
+for($i = 0 ; $i < 10 ; $i++){
+	echo "nihao a ".$i."<br>";
+}
+
+for($i = 0 ; $i < 10 ; $i++){
+	echo "nihao a ".$i."<br>";
+}
+for($i = 0 ; $i < 10 ; $i++){
+	echo "nihao a ".$i."<br>";
+}
+var_dump($i);
+
+
+for($i = 0 ; $i < 10 ; $i++){
+	echo "nihao a ".$i."<br>";
+}
+
+echo "haha just a test for git !!!";
+
